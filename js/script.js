df0e2e10aa537bfaa187e259c6c1a57d38d2a25d
Martin Rodriguez <68254461+littlee5521@users.noreply.github.com>
2023-02-08 00:40:13 -0800

    Finished logic for winning game need to refactor

--- js/script.js
+++ js/script.js
@@ -9,36 +9,131 @@ const playerList = [player1, player2];
 
 const gameboard = (() => {
   //                        Selects buttons and adds listener
-
+  //                        current turn is index of player list
   const buttonlist = document.querySelectorAll(".game-body__game-area");
   let currentTurn = 0;
-
+  //                        find index of button pressed
   const getIndex = (button) => {
     let buttonId = button.id.replace(/[^0-9]/g, "");
+    buttonId = Number(buttonId);
     return buttonId;
   };
 
   buttonlist.forEach((button) => {
     button.addEventListener("click", () => {
       button.textContent = playerList[currentTurn].type;
-      gameStatus.splice(
-        getIndex(button),
-        getIndex(button),
-        playerList[currentTurn].type
-      );
+      gameStatus[getIndex(button)] = playerList[currentTurn].type;
+      if (gamewinner(button) == true) {
+        console.log("I ran");
+      }
       if (currentTurn == 0) {
         currentTurn = 1;
       } else {
         currentTurn = 0;
       }
+      //
     });
   });
 
-  //                        Creates arry for gameboard
+  //                        Creates array for gameboard
 
   const gameStatus = [];
   for (i = 0; i < 9; i++) {
     gameStatus.push(0);
   }
+  //                         forgive me for this
+  const gamewinner = (button) => {
+    let win = "";
+    const horizontal = () => {
+      let counter = 0;
+      for (i = 1; i < 3; i++) {
+        console.log(i + "Whats going on");
+        //spent a hour here because my index was returning a string and not a number smh
+        if (playerList[currentTurn].type == gameStatus[getIndex(button) + i]) {
+          console.log("Hello");
+          counter++;
+        }
+        if (playerList[currentTurn].type == gameStatus[getIndex(button) - i]) {
+          console.log("Hello");
+          counter++;
+        }
+      }
+      if (counter >= 2) {
+        win = true;
+      }
+    };
+    const vertical = () => {
+      let counter = 0;
+      for (i = 1; i < 3; i++) {
+        console.log(i + "Whats going on");
+        //spent a hour here because my index was returning a string and not a number smh
+        if (
+          playerList[currentTurn].type == gameStatus[getIndex(button) + i * 3]
+        ) {
+          console.log("Hello");
+          counter++;
+        }
+        if (
+          playerList[currentTurn].type == gameStatus[getIndex(button) - i * 3]
+        ) {
+          console.log("Hello");
+          counter++;
+        }
+      }
+      if (counter >= 2) {
+        win = true;
+      }
+    };
+    const diagonalL = () => {
+      let counter = 0;
+      for (i = 1; i < 3; i++) {
+        console.log(i + "Whats going on");
+        //spent a hour here because my index was returning a string and not a number smh
+        if (
+          playerList[currentTurn].type == gameStatus[getIndex(button) + i * 4]
+        ) {
+          console.log("Hello");
+          counter++;
+        }
+        if (
+          playerList[currentTurn].type == gameStatus[getIndex(button) - i * 4]
+        ) {
+          console.log("Hello");
+          counter++;
+        }
+      }
+      if (counter >= 2) {
+        win = true;
+      }
+    };
+    const diagonalR = () => {
+      let counter = 0;
+      for (i = 1; i < 3; i++) {
+        console.log(i + "Whats going on");
+        //spent a hour here because my index was returning a string and not a number smh
+        if (
+          playerList[currentTurn].type == gameStatus[getIndex(button) + i * 2]
+        ) {
+          console.log("Hello");
+          counter++;
+        }
+        if (
+          playerList[currentTurn].type == gameStatus[getIndex(button) - i * 2]
+        ) {
+          console.log("Hello");
+          counter++;
+        }
+      }
+      if (counter >= 2) {
+        win = true;
+      }
+    };
+    diagonalR();
+    diagonalL();
+    vertical();
+    horizontal();
+    return win;
+  };
+
   return { gameStatus };
 })();
